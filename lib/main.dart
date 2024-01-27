c83fa549797087e677674f8dd73534cd1736e629
mohamed <mezoattalla123@gmail.com>
2024-01-27 21:12:07 +0200

    Merge remote-tracking branch 'origin/master'

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 import 'package:new_app/pages/home_page.dart';
 
 void main() {
--- lib/services/news-services.dart
+++ lib/services/news-services.dart
@@ -12,11 +12,11 @@ class NewsService {
     Response response = await dio.get(
         'https://newsapi.org/v2/everything?q=$q&apiKey=c5960a887f78413b994e0f5d787c46cb');
 
-    Map<String, dynamic> jsondata = response.data;
+    Map<String, dynamic> jsonData = response.data;
 
-    List<dynamic> articles = jsondata['articles'];
+    List<dynamic> articles = jsonData['articles'];
     List<ArticleModel> articleList = [];
-
+  // chanege the name 
     for (var articale in articles) {
       ArticleModel articleModel = ArticleModel.fromJson(articale);
       articleList.add(articleModel);
